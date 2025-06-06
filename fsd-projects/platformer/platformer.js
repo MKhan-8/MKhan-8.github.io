d9ba125f025f71a5c81878b6cbbff9a348240966
MKhan-8 <muhammadhkhan28@gmail.com>
2025-06-06 15:45:44 +0000

    platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -15,6 +15,7 @@ $(function () {
       //start game
       setInterval(main, 1000 / frameRate);
     }
+    
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
@@ -28,19 +29,40 @@ $(function () {
 
     // TODO 1 - Enable the Grid
     // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
+    createPlatform(320, 480, 220, 100, "navy");
+    createPlatform(650, 610, 200, 100, "red");
+    createPlatform(615, 345, 200, 80), "yellow";
+    createPlatform(900, 215, 200, 100, "orange");
+    createPlatform(1299, 102, 200, 80, "white")
 
 
 
 
     // TODO 3 - Create Collectables
+    
+    createCollectable("max", 100, 0, 1, 1);
+   
+    createCollectable("steve", 400, 0, 1, 1);
+ 
+    createCollectable("grace", 700, 0, 1, 1);
+
+    createCollectable("kennedi", 1000, 0, 1, 1);
+
+ 
+    createCollectable("steve", 1200, 0, 1, 1);
+    createCollectable("max", 1350, 50, 1, 1);
 
 
 
     
     // TODO 4 - Create Cannons
+    createCannon("left", 100, 1000);
+    createCannon("top", 505, 1200);
+    createCannon("bottom", 1200, 2000);
 
 
     
